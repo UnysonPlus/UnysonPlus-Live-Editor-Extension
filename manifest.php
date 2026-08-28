2673a98c5d234158c24010bff74bbcd4da7ccd22
jonmlas <jonmlas@gmail.com>
2026-08-28 23:00:01 +0800

    3.0.0 milestone: modern admin option framework revamp

--- manifest.php
+++ manifest.php
@@ -13,7 +13,7 @@ $manifest['description'] = __(
 	'fw'
 );
 
-$manifest['version']    = '0.2.69';
+$manifest['version']    = '0.2.70';
 $manifest['display']    = true;
 $manifest['standalone'] = true;
 $manifest['thumbnail']  = 'thumbnail.svg';
